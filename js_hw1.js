4e2848892b5841b1a0765e7ebde17a6213918f6c
OksanaVinichuk <OViniTheChuk@gmail.com>
2020-10-27 18:17:47 +0200

    Initial commit

---
+++ js_hw1.js
@@ -0,0 +1,42 @@
+// - присвоить каждому из следующих значений свою переменную: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false Вывести каждую при помощи console.log , alert, document.write
+let greeting = "hello"; console.log(greeting);
+// let owu="owu"; alert(owu);
+// let com="com"; document.write(com);
+// let ukr="ua"; console.log(ukr);
+// let num=1; alert(num);
+// let number=10; document.write(number);
+// let index=-999; console.log(index);
+// let digit=123; alert(digit);
+// let pi=3.14; document.write(pi);
+// let fraction=2.7; console.log(fraction);
+// let multiple=16; alert(multiple);
+// let boolean1=true; document.write(boolean1);
+// let boolean2=false; console.log(boolean2);
+//
+// // - переопределить каждую переменную из задания 1 дав им произвольные значения Вывести каждую при помощи console.log , alert, document.write
+// greeting="hello world"; console.log(greeting);
+// owu="OktenWebUniversity"; alert(owu);
+// com="gmail"; document.write(com);
+// ukr="Ukraine"; console.log(ukr);
+// num=9; alert(num);
+// number=99; document.write(number);
+// index=-999; console.log(index);
+// digit=321; alert(digit);
+// pi=3.141596; document.write(pi);
+// fraction=3.2; console.log(fraction);
+// multiple=25; alert(multiple);
+// boolean1=false; document.write(boolean1);
+// boolean2=true; console.log(boolean2);
+//
+// // - Создать 3 числовых и 3 стринговых константы. Вывести каждую при помощи console.log , alert, document.write
+// const Pi=3.1415; document.write(Pi);
+// const g=9.8; console.log(g)
+// const c=299792458; alert(c)
+// const welcomeGreeting='Welcome!'; console.log(welcomeGreeting);
+// const apple="Яблуко"; document.write(apple);
+// const A8='Samsung Galaxy A8'; alert(A8);
+//
+// // - при помощи 3х разных prompt() получить 3 слова которые являются вашими ФИО. Для фамилии имени и отчества создать разные переменные. Вывести каждую при помощи console.log , alert, document.write
+// let Name=prompt("Напишіть,будь-ласка, Ваше ім'я"); document.write(Name);
+// let middleName=prompt("Напишіть,,будь-ласка, як Вас по-батькові"); console.log(middleName);
+// let lastName=prompt("Напишіть,будь-ласка, Ваше прізвище"); alert(lastName);
